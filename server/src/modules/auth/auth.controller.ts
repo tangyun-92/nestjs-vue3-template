fe4dfe116d8796cfe78b0be210f2551ba4072255
tangyun <tangyun@seaskylight.com>
2025-12-08 15:31:35 +0800

    退出登录不需要鉴权

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -8,23 +8,24 @@ import { ResponseWrapper } from "src/common/response.wrapper";
 @UseGuards(JwtAuthGuard)
 @Controller('auth')
 export class AuthController {
-
-  constructor(
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Public()
   @Post('login')
   async login(@Body() loginDto: LoginDto) {
-    const user = await this.authService.validateUser(loginDto.userName, loginDto.password);
+    const user = await this.authService.validateUser(
+      loginDto.userName,
+      loginDto.password,
+    );
 
     if (!user) {
-      throw new UnauthorizedException('用户名或密码错误')
+      throw new UnauthorizedException('用户名或密码错误');
     }
 
     return this.authService.login(user);
   }
 
+  @Public()
   @Post('logout')
   async logout() {
     return ResponseWrapper.success('', '退出成功');
@@ -36,12 +37,15 @@ export class AuthController {
     user.roles = ['superadmin'];
 
     return {
-      user
+      user,
     };
   }
 
   @Get('check-token')
-  async checkToken(@Request() req, @Headers('authorization') authHeader: string) {
+  async checkToken(
+    @Request() req,
+    @Headers('authorization') authHeader: string,
+  ) {
     // 提取 token
     const token = authHeader?.replace('Bearer ', '');
 
@@ -59,15 +63,14 @@ export class AuthController {
           expiresAt: expirationDate,
           isExpired: isExpired,
           timeToExpiry: decoded.payload.exp - now,
-          payload: decoded.payload
-        }
+          payload: decoded.payload,
+        },
       };
     } catch (error) {
       return {
         error: 'Invalid token',
-        message: error.message
+        message: error.message,
       };
     }
   }
-
 }
